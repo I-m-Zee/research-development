5a7b9e252f82d6f2641ec3eb936bc6281e1586f0
I-m-Zee <bc090402389@gmail.com>
2023-12-28 12:03:03 +0500

    Add New component for table

---
+++ src/app/primeng-practice/revers-row-in-table/revers-row-in-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-revers-row-in-table',
+  templateUrl: './revers-row-in-table.component.html',
+  styleUrls: ['./revers-row-in-table.component.scss']
+})
+export class ReversRowInTableComponent {
+
+  
+
+}
