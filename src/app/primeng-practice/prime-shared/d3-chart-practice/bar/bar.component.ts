f1bc2ccf739fc20cae4d903b334320260fc96e0c
I-m-Zee <bc090402389@gmail.com>
2024-02-24 13:05:30 +0500

    Convert to smaller functions

--- src/app/primeng-practice/prime-shared/d3-chart-practice/bar/bar.component.ts
+++ src/app/primeng-practice/prime-shared/d3-chart-practice/bar/bar.component.ts
@@ -41,7 +41,7 @@ export class BarComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy
     // }
     if (changes['chartData'].previousValue) {
       this.createSvg();
-      this.drawBars(this.data);
+      // this.drawBars(this.data);
     }
   }
 
@@ -50,75 +50,137 @@ export class BarComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy
 
   ngAfterViewInit(): void {
     this.createSvg();
-    this.drawBars(this.data);
+    // this.drawBars(this.data);
     // this.zone.run(() => {
     //   console.log('enabled time travel');
     // });
   }
 
   private createSvg(): void {
-    if (this.chart != undefined && this.chart.nativeElement) {
-      this.renderer.setProperty(this.chart.nativeElement, 'innerHTML', "")
+    // if (this.chart != undefined && this.chart.nativeElement) {
+    //   this.renderer.setProperty(this.chart.nativeElement, 'innerHTML', "")
+    // }
+    if (this.svg != undefined) {
+      // this.svg.selectAll("bars").remove();
+      d3.selectAll("bars").remove();
+      this.updateBars();
+    } else {
+      this.svg = d3.select(this.chart.nativeElement as HTMLDivElement)
+        .append("svg")
+        .attr("width", this.chartData.width + (this.chartData.marginTop * 5))
+        .attr("height", this.chartData.height + (this.chartData.marginRight * 5))
+        .append("g")
+        .attr("transform", "translate(" + this.chartData.marginTop + "," + this.chartData.marginRight + ")");
+      this.drawXAxis();
+      this.drawYAxis();
+      this.updateBars();
     }
-    this.svg = d3.select(this.chart.nativeElement as HTMLDivElement)
-      .append("svg")
-      .attr("width", this.chartData.width + (this.chartData.marginTop * 5))
-      .attr("height", this.chartData.height + (this.chartData.marginRight * 5))
-      .append("g")
-      .attr("transform", "translate(" + this.chartData.marginTop + "," + this.chartData.marginRight + ")");
 
   }
 
 
-  private drawBars(data: any[]): void {
+  // private drawBars(data: any[]): void {
+  //   // Create the X-axis band scale
+  //   const x = d3.scaleBand()
+  //     .range([0, this.chartData.width])
+  //     .domain(data.map(d => d.Framework))
+  //     .padding(0.2);
+
+  //   // Draw the X-axis on the DOM
+  //   this.svg.append("g")
+  //     .attr("transform", "translate(0," + this.chartData.height + ")")
+  //     .call(d3.axisBottom(x))
+  //     .selectAll("text")
+  //     .attr("transform", "translate(-10,0)rotate(-45)")
+  //     .style("text-anchor", "end")
+  //   // .transition()
+  //   // .ease(d3.easeCircleInOut)
+  //   // .duration(3000);
+
+  //   // Create the Y-axis band scale
+  //   const y = d3.scaleLinear()
+  //     .domain([0, 200000])
+  //     .range([this.chartData.height, 0]);
+
+  //   // Draw the Y-axis on the DOM
+  //   this.svg.append("g")
+  //     .attr("transform", "translate(0, 10)")
+  //     .call(d3.axisLeft(y));
+  //   // var y_axis = d3.axisLeft(y)
+
+  //   // this.svg.append("g")
+  //   //   .attr("transform", "translate(50, 10)")
+  //   //   .call(y_axis);
+
+  //   // Create and fill the bars
+  //   this.svg.selectAll("bars")
+  //     .data(data)
+  //     .enter()
+  //     .append("rect")
+  //     .transition(d3.easeBack)
+  //     .duration(1000)
+  //     .attr("x", (d: any) => x(d.Framework))
+  //     .attr("y", (d: any) => y(d.Stars))
+  //     .attr("width", x.bandwidth())
+  //     .attr("height", (d: any) => this.chartData.height - y(d.Stars))
+  //     .attr("fill", "#d04a35");
+  // }
+
+  ngOnDestroy(): void {
+    console.log("Bar Chart destroyed")
+  }
+
+  updateBars() {
+    // Create and fill the bars
+    this.svg.selectAll("bars")
+      .data(this.data)
+      .enter()
+      .append("rect")
+      .transition(d3.easeBack)
+      .duration(1000)
+      .attr("x", (d: any) => this.xAxis(d.Framework))
+      .attr("y", (d: any) => this.yAxis(d.Stars))
+      .attr("width", this.xAxis.bandwidth())
+      .attr("height", (d: any) => this.chartData.height - this.yAxis(d.Stars))
+      .attr("fill", "#d04a35");
+  }
+
+  get xAxis() {
     // Create the X-axis band scale
-    const x = d3.scaleBand()
+    return d3.scaleBand()
       .range([0, this.chartData.width])
       .domain(data.map(d => d.Framework))
       .padding(0.2);
+  }
 
+  get yAxis() {
+    // Create the Y-axis band scale
+    return d3.scaleLinear()
+      .domain([0, 200000])
+      .range([this.chartData.height, 0]);
+  }
+
+  drawXAxis() {
     // Draw the X-axis on the DOM
     this.svg.append("g")
       .attr("transform", "translate(0," + this.chartData.height + ")")
-      .call(d3.axisBottom(x))
+      .call(d3.axisBottom(this.xAxis))
       .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end")
-    // .transition()
-    // .ease(d3.easeCircleInOut)
-    // .duration(3000);
+  }
 
-    // Create the Y-axis band scale
-    const y = d3.scaleLinear()
-      .domain([0, 200000])
-      .range([this.chartData.height, 0]);
-
+  drawYAxis() {
     // Draw the Y-axis on the DOM
     this.svg.append("g")
       .attr("transform", "translate(0, 10)")
-      .call(d3.axisLeft(y));
+      .call(d3.axisLeft(this.yAxis));
     // var y_axis = d3.axisLeft(y)
 
     // this.svg.append("g")
     //   .attr("transform", "translate(50, 10)")
     //   .call(y_axis);
-
-    // Create and fill the bars
-    this.svg.selectAll("bars")
-      .data(data)
-      .enter()
-      .append("rect")
-      .transition(d3.easeBack)
-      .duration(1000)
-      .attr("x", (d: any) => x(d.Framework))
-      .attr("y", (d: any) => y(d.Stars))
-      .attr("width", x.bandwidth())
-      .attr("height", (d: any) => this.chartData.height - y(d.Stars))
-      .attr("fill", "#d04a35");
   }
 
-  ngOnDestroy(): void {
-    console.log("Bar Chart destroyed")
-  }
 
 }
